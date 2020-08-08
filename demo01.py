39271b4d9553310524a687be1625401c305af7e3
sophia <1040616989@qq.com>
2020-08-08 18:39:13 +0800

    app定位元素 h5页面定位元素

--- demo01.py
+++ demo01.py
@@ -13,11 +13,41 @@ des = {
     'resetKeyboard': True,
 }
 driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub', des)
-driver.find_element_by_id('com.android.calculator2:id/digit_1').click()
-driver.find_element_by_id('com.android.calculator2:id/digit_0').click()
-time.sleep(2)
-driver.find_element_by_accessibility_id('plus').click()
-driver.find_element_by_id('com.android.calculator2:id/digit_5').click()
-time.sleep(2)
-driver.find_element_by_accessibility_id('equals').click()
+# driver.find_element_by_id('com.android.calculator2:id/digit_1').click()      #(取resource-id、id、name)
+# driver.find_element_by_id('com.android.calculator2:id/digit_0').click()
+# time.sleep(2)
+# driver.find_element_by_accessibility_id('plus').click()     #(取content-desc)
+# driver.find_element_by_id('com.android.calculator2:id/digit_5').click()
+# time.sleep(2)
+# driver.find_element_by_accessibility_id('equals').click()
+
+# driver.find_element_by_xpath('//android.widget.Button[@text=9]').click()
+# driver.find_element_by_xpath('//android.widget.Button[@resource-id="com.android.calculator2:id/op_add"]').click()
+# driver.find_element_by_xpath('//android.widget.Button[@resource-id="com.android.calculator2:id/digit_3"]').click()
+# driver.find_element_by_xpath('//android.widget.Button[@content-desc="equals" and @index="5"]').click()
+
+# driver.find_element_by_xpath('//android.widget.Button[@text=9]').click()
+# driver.find_element_by_xpath('//android.widget.Button[contains(@resource-id,":id/del")]').click()
+# driver.find_element_by_xpath('//android.widget.Button[ends-with(@resource-id,"/digit_4")]').click()
+
+
+#
+# driver.find_element_by_android_uiautomator('text("9")').click()
+# driver.find_element_by_android_uiautomator('new UiSelector().textContains("8")').click()
+#
+# driver.find_element_by_android_uiautomator('resourceId("com.android.calculator2:id/digit_6")').click()
+# driver.find_element_by_android_uiautomator('new UiSelector().resourceId("com.android.calculator2:id/digit_6")').click()
+#
+# driver.find_element_by_android_uiautomator('className("android.widget.Button")').click()
+# driver.find_element_by_android_uiautomator('new UiSelector().className("android.widget.Button").text("9")').click()
+#
+# driver.find_element_by_android_uiautomator('description("delete")').click()
+# driver.find_element_by_android_uiautomator('new UiSelector().description("delete")').click()
+
+# 后代元素定位
+# driver.find_element_by_android_uiautomator('new UiSelector().resourceId("com.android.calculator2:iad/pad_numeric").childSelector(className("android.widget.Button").instance(1))').click()
+
+# 兄弟元素定位
+# driver.find_element_by_android_uiautomator('new UiSelector().resourceId("com.android.calculator2:id/digit_9")').click()
+driver.find_element_by_android_uiautomator('new UiSelector().resourceId("com.android.calculator2:id/digit_9").fromParent(text("3"))').click()
 
